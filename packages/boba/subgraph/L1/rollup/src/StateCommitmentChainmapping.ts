de9ab40994738c54fa4faca61f4543c48898313c
cby3149 <cby3149@gmail.com>
2022-05-12 15:43:26 -0700

    Update name

--- packages/boba/subgraph/L1/rollup/src/StateCommitmentChainmapping.ts
+++ packages/boba/subgraph/L1/rollup/src/StateCommitmentChainmapping.ts
@@ -6,11 +6,11 @@ export function handleStateCommitmentChain(event: StateBatchAppended): void {
   const id = event.transaction.hash.toHex()
   const entity = new StateBatchAppendedEntity(id)
   entity.id = id
-  entity.batchIndex = event.params._batchIndex
-  entity.batchRoot = event.params._batchRoot
-  entity.batchSize = event.params._batchSize
-  entity.prevTotalElements = event.params._prevTotalElements
-  entity.extraData = event.params._extraData
+  entity._batchIndex = event.params._batchIndex
+  entity._batchRoot = event.params._batchRoot
+  entity._batchSize = event.params._batchSize
+  entity._prevTotalElements = event.params._prevTotalElements
+  entity._extraData = event.params._extraData
   entity.blockNumber = event.block.number
   entity.transactionHash = event.transaction.hash
 
