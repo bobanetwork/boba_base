cc37af606da70b39e24f017210325209288d9eae
cby3149 <cby3149@gmail.com>
2022-05-12 14:59:32 -0700

    Fix entity

--- packages/boba/subgraph/L1/rollup/src/StateCommitmentChainmapping.ts
+++ packages/boba/subgraph/L1/rollup/src/StateCommitmentChainmapping.ts
@@ -2,16 +2,10 @@ import { StateBatchAppended } from '../generated/StateCommitmentChain/StateCommi
 import { StateBatchAppendedEntity } from '../generated/schema'
 
 export function handleStateCommitmentChain(event: StateBatchAppended): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = StateBatchAppendedEntity.load(event.transaction.from.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new StateBatchAppendedEntity(event.transaction.from.toHex())
-  }
-
+  const id = event.transaction.hash.toHex()
+  const entity = new StateBatchAppendedEntity(id)
+  entity.id = id
   entity.batchIndex = event.params._batchIndex
   entity.batchRoot = event.params._batchRoot
   entity.batchSize = event.params._batchSize
