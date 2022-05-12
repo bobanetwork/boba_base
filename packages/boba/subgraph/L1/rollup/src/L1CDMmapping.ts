c9a7a4f99019bed40b864586ed9b55f67e933141
cby3149 <cby3149@gmail.com>
2022-05-12 13:48:37 -0700

    Add rollup subgraph

---
+++ packages/boba/subgraph/L1/rollup/src/L1CDMmapping.ts
@@ -0,0 +1,36 @@
+import {
+  FailedRelayedMessage,
+  RelayedMessage,
+} from "../generated/L1CrossDomainMessenger/L1CrossDomainMessenger"
+import { RelayedMessageEntity } from "../generated/schema"
+
+export function handleFailedRelayedMessage(event: FailedRelayedMessage): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = RelayedMessageEntity.load(event.transaction.from.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new RelayedMessageEntity(event.transaction.from.toHex())
+  }
+
+  entity.isSuccess = false
+  entity.msgHash = event.params.msgHash
+  entity.blockNumber = event.block.number
+
+  entity.save()
+}
+
+export function handleRelayedMessage(event: RelayedMessage): void {
+  let entity = RelayedMessageEntity.load(event.transaction.from.toHex())
+  if (!entity) {
+    entity = new RelayedMessageEntity(event.transaction.from.toHex())
+  }
+
+  entity.isSuccess = true
+  entity.msgHash = event.params.msgHash
+  entity.blockNumber = event.block.number
+
+  entity.save()
+}
---
+++ packages/boba/subgraph/L1/rollup/src/StateCommitmentChainmapping.ts
@@ -0,0 +1,23 @@
+import { StateBatchAppended } from '../generated/StateCommitmentChain/StateCommitmentChain'
+import { StateBatchAppendedEntity } from '../generated/schema'
+
+export function handleStateCommitmentChain(event: StateBatchAppended): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = StateBatchAppendedEntity.load(event.transaction.from.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new StateBatchAppendedEntity(event.transaction.from.toHex())
+  }
+
+  entity.batchIndex = event.params._batchIndex
+  entity.batchRoot = event.params._batchRoot
+  entity.batchSize = event.params._batchSize
+  entity.prevTotalElements = event.params._prevTotalElements
+  entity.extraData = event.params._extraData
+  entity.blockNumber = event.block.number
+
+  entity.save()
+}
