0ddcfbfbf545435ea4b2cdb56f4d02a5d320b201
cby3149 <cby3149@gmail.com>
2022-06-22 14:07:05 -0700

    Use one branch for multiple l2s

---
+++ ops/scripts/build-ci-moonbeam.sh
@@ -0,0 +1,10 @@
+yarn
+yarn build
+
+docker-compose build -f docker-compose-moonbeam.yml --parallel -- builder l2geth l1_chain
+docker-compose build -f docker-compose-moonbeam.yml --parallel -- deployer dtl batch_submitter relayer integration_tests
+docker-compose build -f docker-compose-moonbeam.yml --parallel -- boba_message-relayer-fast boba_deployer fraud-detector
+
+docker ps
+
+wait
